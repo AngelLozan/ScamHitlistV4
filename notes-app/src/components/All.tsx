c3c30d891e20205301ceda7dfed5e128f0fcf97e
Scott Lozano <scottloz@gmail.com>
2024-02-08 09:00:43 +0000

    correct mui location && add customizable table props

--- notes-app/src/components/All.tsx
+++ notes-app/src/components/All.tsx
@@ -1,10 +1,16 @@
-import BasicTable from "./BasicTable";
+import DataTable from "./DataTable";
+
+
+const rows = [
+  { id: 1, url: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, url: 'Lannister', firstName: 'Cersei', age: 42 },
+];
 
 const All = () => {
   return (
     <div className="p3 m-3">
-      <h1>All</h1>
-      <BasicTable />
+      <h1 className="text-center m-3">All Iocs</h1>
+      <DataTable rows = {rows}/>
     </div>
   );
 };
